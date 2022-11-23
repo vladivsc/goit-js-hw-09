81a45d59819b32ab4620148f2d41c7d5bd9147bd
Vladyslav <101827554+vladivsc@users.noreply.github.com>
2022-11-23 17:46:05 +0100

    Update 01-color-switcher.js
    finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let timerId = null;
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(() => changeBodyColor(), 1000);
+
+  refs.startBtn.setAttribute('disabled', 'disabled');
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function changeBodyColor() {
+  const randomColor = getRandomHexColor();
+
+  refs.body.style.backgroundColor = randomColor;
+}
